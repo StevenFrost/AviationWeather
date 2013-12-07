8d3063531768e76ace853adfec49ee71c57c0d04
Steven Frost <steve.frost.private@gmail.com>
2013-12-07 12:50:28 +0000

    Changed header file extensions

---
+++ Source/Meteorology.hpp
@@ -0,0 +1,121 @@
+/**
+* Module:		Meteorology.h
+* Author(s):	Steven Frost
+* Description:	This module defines the Weather class including general meteorology methods,
+*				meteorology-releated enumerations and type definitions for different meteorology
+*				properties.
+*
+* References:  (1) http://en.wikipedia.org/wiki/Weather
+*
+* Todo:        (1) Complete type definitions
+*              (2) Complete enumerated list of cloud type and cover
+*/
+
+#ifndef METEORLOGY_H
+#define METEORLOGY_H
+
+namespace Meteorology {
+	/**
+	* Typedefs
+	*/
+	typedef double Altimeter;
+	typedef double Dewpoint;
+	typedef double Visibility;
+	typedef double Temperature;
+
+	/**
+	* WeatherIntensity
+	*   Enumerated list of weather intesity options. Each element is
+	*   denoted by `-`, ``, `+` and `VC` respectively.
+	*
+	* See: Reference (1), Table 12-2
+	*/
+	typedef enum WeatherIntensity {
+		WI_NONE,
+		WI_LIGHT,
+		WI_MODERATE,
+		WI_HEAVY,
+		WI_IN_VICINITY
+	};
+
+	/**
+	* WeatherDescriptor
+	*   Description of the present weather in a weather group. Only one
+	*   element can be active in a weather group.
+	*
+	* See: Reference (1), Table 12-2
+	*/
+	typedef enum WeatherDescriptor {
+		WD_NONE,
+		WD_SHALLOW,
+		WD_PARTIAL,
+		WD_PATCHES,
+		WD_LOW_DRIFTING,
+		WD_BLOWING,
+		WD_SHOWERS,
+		WD_THUNDERSTORM,
+		WD_FREEZING
+	};
+
+	/**
+	* WeatherPhenomena
+	*   Enumerated list of valid weather phenomena represented in METAR
+	*   reports. Up to three can be used in one weather group.
+	*
+	* See: Reference (1), Table 12-2
+	*/
+	typedef enum WeatherPhenomena {
+		WP_DRIZZLE,
+		WP_RAIN,
+		WP_SNOW,
+		WP_SNOW_GRAINS,
+		WP_ICE_CRYSTALS,
+		WP_ICE_PELLETS,
+		WP_HAIL,
+		WP_SMALL_HAIL,
+		WP_MIST,
+		WP_FOG,
+		WP_SMOKE,
+		WP_VOLCANIC_ASH,
+		WP_WIDESPREAD_DUST,
+		WP_SAND,
+		WP_HAZE,
+		WP_SPRAY,
+		WP_WELL_DEVELOPED_DUST_WHIRLS,
+		WP_SQUALLS,
+		WP_FUNNEL_CLOUD_TORNADO_WATERSPOUT,
+		WP_SANDSTORM,
+		WP_DUSTSTORM,
+		WP_UNKNOWN_PRECIPITATION
+	};
+
+	/**
+	* SkyCover
+	*   Enumerated list of valid cloud layer coverage amounts.
+	*
+	* See: Reference (1), Table 12-3
+	*/
+	typedef enum SkyCover {
+		SC_VERTICAL_VISIBILITY,
+		SC_CLEAR,
+		SC_FEW,
+		SC_SCATTERED,
+		SC_BROKEN,
+		SC_OVERCAST
+	};
+
+	/**
+	* SkyCoverClouds
+	*   For manually edited METAR reports, cloud types may be appended
+	*   to the Sky Cover group. If this is available, the enumeration
+	*   will be set to a value other than NONE.
+	*
+	* See: Reference (1), Paragraph under Table 12-4
+	*/
+	typedef enum SkyCoverClouds {
+		SCC_NONE,
+		SCC_CUMULONIMBUS,
+		SCC_TOWERING_CUMULUS
+	};
+}
+#endif // !METEORLOGY_H
--- Source/stdafx.h
+++
@@ -1,8 +0,0 @@
-#ifndef STDAFX_H
-#define STDAFX_H
-
-#include "targetver.h"
-
-#define WIN32_LEAN_AND_MEAN
-
-#endif // !STDAFX_H
